0535a3067a2ca2ec5911bf2fb729fdfd524a17f4
lusan23 <santosluanstudy@gmail.com>
2024-01-11 12:46:11 -0300

    creates table's headers funcionality

--- client_side/scripts/build_my_notes_layout.js
+++ client_side/scripts/build_my_notes_layout.js
@@ -1,5 +1,17 @@
 export function buildTable(containerId) {
     const container = document.getElementById(containerId);
     const tb = document.createElement("table");
+  
+
+    //building the headers
+    const headersArr = ["Subject", "Content", "Creation Date"];
+    const thead = document.createElement("thead");
+    let row = thead.insertRow();
+    for (let header of headersArr) {
+        let tmpCell = row.insertCell();
+        tmpCell.textContent = header;
+    }
+    tb.appendChild(thead);
+    
     container.appendChild(tb);
 }
